1c8f0db212f76c0c3fd5b73f4d366e6f2bd6871b
Samuelgarza-cpu <78004493+Samuelgarza-cpu@users.noreply.github.com>
2023-10-20 20:20:06 -0600

    se corrigue middleware en el return

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,7 +16,7 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        $response = $next($request);
         $response->headers->set('Access-Control-Allow-Origin', '*');
         $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
