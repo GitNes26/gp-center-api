b8b5662711ce5842a2971b0258c66f5a1ac563d5
Nestor <nestorpuentesin@gmail.com>
2024-02-06 12:28:37 -0600

    iba a implementar que solo muestre conductores correspondientes segun el director

--- app/Http/Controllers/DirectorController.php
+++ app/Http/Controllers/DirectorController.php
@@ -9,6 +9,7 @@ use App\Models\DirectorView;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
