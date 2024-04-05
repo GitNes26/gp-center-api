0809aaaeeeedbac69ff58bf01df3c731842f3574
Nestor <nestorpuentesin@gmail.com>
2024-04-05 15:25:22 -0600

    modifico el index de vouchers para mostrar los vales despues de ALTA a supervisores y se agrego el campo img_stamp a solicitadores de vales y sus vistas

--- database/migrations/2023_12_00_165045_create_vouchers_view.php
+++ database/migrations/2023_12_00_165045_create_vouchers_view.php
@@ -32,15 +32,15 @@ return new class extends Migration
 
         DB::statement("
         CREATE OR REPLACE VIEW vouchers_view AS
-        SELECT vvp.*, u.username 'requested_fullname', 'JEFE DEL DEPARTAMENTO DE CONTROL VEHÍCULAR' as 'workstation', NULL 'img_firm',
+        SELECT vvp.*, u.username 'requested_fullname', 'JEFE DEL DEPARTAMENTO DE CONTROL VEHÍCULAR' as 'workstation', NULL 'img_firm', NULL 'img_stamp', 
         NULL 'requested_payroll_number', NULL 'requested_department', NULL 'requested_phone'
         FROM vouchers_view_prev vvp INNER JOIN users u ON vvp.requested_by=u.id WHERE vvp.requested_role_id IN (1,7)
         UNION
-        SELECT vvp.*, dv.full_name 'requested_fullname', CONCAT('DIRECTOR DEL DEPARTAMENTO DE ',dv.department) 'workstation', dv.img_firm,
+        SELECT vvp.*, dv.full_name 'requested_fullname', CONCAT('DIRECTOR DEL DEPARTAMENTO DE ',dv.department) 'workstation', dv.img_firm, NULL 'img_stamp',
         dv.payroll_number 'requested_payroll_number', dv.department 'requested_department', dv.phone 'requested_phone'
         FROM vouchers_view_prev vvp INNER JOIN directors_view dv ON vvp.requested_by=dv.user_id WHERE vvp.requested_role_id=5
         UNION
-        SELECT vvp.*, vrv.full_name 'requested_fullname', CONCAT('DIRECTOR DEL DEPARTAMENTO DE ',vrv.department) 'workstation', vrv.img_firm,
+        SELECT vvp.*, vrv.full_name 'requested_fullname', CONCAT('DIRECTOR DEL DEPARTAMENTO DE ',vrv.department) 'workstation', vrv.img_firm, vrv.img_stamp,
         vrv.payroll_number 'requested_payroll_number', vrv.department 'requested_department', vrv.phone 'requested_phone'
         FROM vouchers_view_prev vvp INNER JOIN voucher_requesters_view vrv ON vvp.requested_by=vrv.user_id WHERE vvp.requested_role_id=8
         ;
